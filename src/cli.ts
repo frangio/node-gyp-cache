4e079a512a417d39c38d49d5c6c9d1b4d1a7693b
Francisco Giordano <frangio.1@gmail.com>
2019-11-23 23:03:11 -0300

    rewrite in typescript and support all node-gyp subcommands

---
+++ src/cli.ts
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import gyp from './gyp';
+
+(async function () {
+  const command = process.argv[2];
+  const options = process.argv.slice(3);
+
+  try {
+    await gyp(command, options);
+  } catch (e) {
+    console.error(`nicer-node-gyp: ${e.message}`);
+    process.exit(1);
+  }
+})();
---
+++ src/gyp.ts
@@ -0,0 +1,183 @@
+import proc from 'child_process';
+import path from 'path';
+import stream from 'stream';
+import { constants as fsContants } from 'fs';
+import fs from 'fs-extra';
+import envPaths from 'env-paths';
+import ora from 'ora';
+import { promisify } from 'util';
+
+const pipeline = promisify(stream.pipeline);
+
+const paths = envPaths('nicer-node-gyp', { suffix: '' });
+
+export default async function gyp(command: string, options: string[]) {
+  const name = process.env.npm_package_name;
+  const version = process.env.npm_package_version;
+
+  if (name === undefined || version === undefined) {
+    throw new Error('Must be invoked through npm');
+  }
+
+  const logDir = path.join(paths.temp, name, version);
+  await fs.ensureDir(logDir);
+  const logPath = path.join(logDir, `${Date.now()}.log`);
+  const logFile = fs.createWriteStream(logPath);
+  try {
+    await new Gyp(name, version, command, options, logFile).run();
+  } catch (e) {
+    logFile.end();
+    throw new Error(`logs saved at ${logPath}`);
+    console.error();
+  }
+}
+
+class Gyp {
+  private readonly useCache: boolean;
+  private spinner?: ora.Ora;
+
+  constructor(
+    readonly name: string,
+    readonly version: string,
+    readonly command: string,
+    readonly options: string[],
+    readonly logFile: fs.WriteStream,
+  ) {
+    this.useCache = process.cwd().split(path.sep).includes('node_modules');
+  }
+
+  async run(): Promise<void> {
+    try {
+      await this._run();
+    } catch (e) {
+      await this.log(e.message);
+      this.spinner?.fail();
+      throw e;
+    }
+  }
+
+  async _run(): Promise<void> {
+    if (this.isBuild) {
+      this.spinner = ora('nicer-node-gyp').start();
+
+      const restoredFromCache = this.useCache && await this.cacheGet();
+
+      if (!restoredFromCache) {
+        await this.cmd();
+      }
+
+      if (this.useCache && !restoredFromCache) {
+        await this.cachePut();
+      }
+
+      this.spinner.succeed();
+    } else {
+      if (this.command === 'clean') {
+        await this.cachePurge();
+      }
+
+      await this.cmd();
+    }
+  }
+
+  async cmd() {
+    await this.log('running node-gyp');
+
+    const paths = [
+      'lib/node_modules/npm/node_modules',
+      'share',
+    ];
+
+    const bin = await find(
+      paths.map(p =>
+        path.join(process.argv[0], '../..', p, 'node-gyp/bin/node-gyp.js')
+      ),
+      fs.pathExists,
+    );
+
+    if (bin === undefined) {
+      throw new Error('node-gyp executable not found');
+    }
+
+    const child = proc.spawn(
+      bin,
+      [this.command, ...this.options],
+      { stdio: ['inherit', this.stdout, this.stderr] },
+    );
+
+    return new Promise((resolve, reject) => {
+      child.on('exit', (code) => {
+        if (code === 0) {
+          this.log('finished running node-gyp').then(resolve);
+        } else {
+          reject(new Error('error running node-gyp'));
+        }
+      });
+    });
+  }
+
+  async cacheGet(): Promise<boolean> {
+    try {
+      await fs.access(this.cachePath, fsContants.R_OK | fsContants.W_OK);
+      await this.log('restoring build from cache');
+      await fs.copy(this.cachePath, 'build');
+      await this.log('restored build from cache');
+      return true;
+    } catch (e) {
+      await this.log('failed to restore build from cache');
+      return false;
+    }
+  }
+
+  async cachePut(): Promise<void> {
+    await this.log('storing build in cache');
+    await fs.copy('build', this.cachePath);
+    await this.log('rebuilt and cached');
+  }
+
+  async cachePurge(): Promise<void> {
+    await this.log('purging cache');
+    await fs.remove(this.cachePath);
+    await this.log('rebuilt and cached');
+  }
+
+  async log(msg: string): Promise<void> {
+    if (this.spinner !== undefined) {
+      this.spinner.text = `nicer-node-gyp: ${msg}`;
+    }
+
+    return new Promise((resolve, reject) => {
+      this.logFile.write(msg + '\n', (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  get stderr(): fs.WriteStream | 'inherit' {
+    return this.isBuild ? this.logFile : 'inherit';
+  }
+
+  get stdout(): fs.WriteStream | 'inherit' {
+    return this.isBuild ? this.logFile : 'inherit';
+  }
+
+  get isBuild(): boolean {
+    return this.command === 'build' || this.command === 'rebuild';
+  }
+
+  get cachePath(): string {
+    return path.join(paths.cache, this.name, this.version);
+  }
+}
+
+async function find<T>(array: T[], pred: (t: T) => Promise<boolean>): Promise<T | undefined> {
+  for (const elem of array) {
+    if (await pred(elem)) {
+      return elem;
+    }
+  }
+}
